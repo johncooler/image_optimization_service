63217ebe885fb0de31237016e70ec53c8d703058
johncooler <lascapita@gmail.com>
2023-08-20 16:36:11 +0300

    Add logging stuff

---
+++ src/common_stuff/logger.py
@@ -0,0 +1,24 @@
+import logging
+
+
+def get_logger(name):
+    # create logger
+    return logging.getLogger(name)
+
+
+def setup_logger(logger: logging.Logger):
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(f'{logger.name}_app.log')
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
